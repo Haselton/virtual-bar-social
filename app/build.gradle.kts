8215784354e02152bcde699df6185f345236a001
Haselton <147449208+Haselton@users.noreply.github.com>
2026-09-18 02:26:19 -0700

    Build virtual bar social prototype

---
+++ app/build.gradle.kts
@@ -0,0 +1,3 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+android { namespace = "com.haseltonmediagroup.virtualbarsocial"; compileSdk = 35
+ defaultConfig { applicationId = "com.haseltonmediagroup.virtualbarsocial"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "0.1-prototype" } }
